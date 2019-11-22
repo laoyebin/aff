7514d0e552e873a2c18061314462d8be301850a5
laoyebin <laoyebin@gmail.com>
2019-11-23 00:59:26 +0800

    Signed-off-by: laoyebin <laoyebin@gmail.com>

--- js/adv/random-adv/300x250-affcn.js
+++
@@ -1,4 +0,0 @@
-var p=9;
-var q=Math.floor(Math.random()*p+1);
-document.writeln("<a href=https://trk.gs/easyforex target=_blank><img src=https://dev.tencent.com/u/affcn/p/files/git/raw/master/img/adv/forex/easyforex/300x250-"+q+".gif  border=0></a>");
-
