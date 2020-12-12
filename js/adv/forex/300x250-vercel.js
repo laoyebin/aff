4afa381109c472dfe32e3d9a7b99b2936d12bf9e
laoyebin <laoyebin@gmail.com>
2020-12-12 13:00:53 +0800

    Signed-off-by: laoyebin <laoyebin@gmail.com>

---
+++ js/adv/forex/300x250-vercel.js
@@ -0,0 +1,4 @@
+var p=9;
+var q=Math.floor(Math.random()*p+1);
+document.writeln("<a href=https://trk.gs/easyforex target=_blank><img src=https://git.laoyebin.com/img/adv/forex/easyforex/300x250-"+q+".gif  border=0></a>");
+
