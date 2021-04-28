4d9c12cefdb86efac0ccd7b3fa090edeae47c8dd
laoyebin <laoyebin@gmail.com>
2021-04-28 13:05:39 +0800

    Signed-off-by: laoyebin <laoyebin@gmail.com>

---
+++ js/adv/forex/300x250-exness-qq.js
@@ -0,0 +1,4 @@
+var p=9;
+var q=Math.floor(Math.random()*p+1);
+document.writeln("<a href=https://trk.gs/exness target=_blank><img src=https://laoyebin.coding.net/p/files/d/files/git/raw/master/img/adv/forex/exness/300x250-"+q+".png  border=0></a>");
+
