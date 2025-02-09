c7f8f369a0eea9c45bfd0ad0d966ac36a261d9c7
laoyebin <laoyebin@gmail.com>
2025-02-09 15:47:57 +0800

    20250209

--- js/adv/forex/728x90-ef.js
+++ js/adv/forex/728x90-ef.js
@@ -1,4 +1,4 @@
 var p=9;
 var q=Math.floor(Math.random()*p+1);
-document.writeln("<a href=https://trk.gs/easyforex target=_blank><img src=https://laoyebin.com/img/adv/forex/easyforex/728x90-"+q+".gif  border=0></a>");
+document.writeln("<a href=https://trk.laoyebin.com/easyforex target=_blank><img src=https://laoyebin.com/img/adv/forex/easyforex/728x90-"+q+".gif  border=0></a>");
 
--- js/adv/forex/728x90-exness.js
+++ js/adv/forex/728x90-exness.js
@@ -1,4 +1,4 @@
 var p=9;
 var q=Math.floor(Math.random()*p+1);
-document.writeln("<a href=https://trk.gs/exness target=_blank><img src=https://laoyebin.com/img/adv/forex/exness/728x90-"+q+".png  border=0></a>");
+document.writeln("<a href=https://trk.laoyebin.com/exness target=_blank><img src=https://laoyebin.com/img/adv/forex/exness/728x90-"+q+".png  border=0></a>");
 
